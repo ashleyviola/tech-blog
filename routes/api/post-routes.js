21ce93ae005e8162ad2cad23a0b1e812fed2afe2
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-01-24 19:17:31 -0500

    added comment routes

---
+++ routes/api/post-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+
+//get all posts 
+
+//get single post 
+
+//create new post 
+
+//update post 
+
+//delete post 
+
+module.exports = router;
