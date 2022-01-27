671d18267d1a0dd678fa2c3268fedaf3f4315e60
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-01-27 18:56:44 -0500

    added initial dashboard route

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -87,5 +87,13 @@ router.get('/post/:id', (req,res) => {
 });
 
 //login 
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+})
 
 module.exports = router;
