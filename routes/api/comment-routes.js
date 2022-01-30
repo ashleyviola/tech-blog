8e5b0fc57ec9606abbaaa5a7cbf1d49721a5776d
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-01-29 21:53:50 -0500

    added comment functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -12,18 +12,18 @@ router.get('/', (req,res) => {
 })
 //create comment 
 router.post('/', (req,res) => {
-    if(req.session){
-        Comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
-            user_id: req.session.user_id
-        })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-        });
-    }
+    // if(req.session){
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+    });
+    // }
 })
 //delete comment 
 router.delete('./:id', (req, res) => {
