ee1d59aa04352280df53a2f1b60a4d25f857f459
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-01-29 20:33:00 -0500

    added logout functionality

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-    checkPassword(loginPw){
+    checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
@@ -37,12 +37,24 @@ User.init(
         }
     },
     {
-        sequelize, 
+        hooks: {
+          // set up beforeCreate lifecycle "hook" functionality
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+    
+          async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+        },
+        sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user'
-    }
+      }
 );
 
 module.exports = User;
